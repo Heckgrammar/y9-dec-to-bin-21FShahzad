c18f88343fc804f8a46ef5e3b33caac796c16dc1
Fatima Shahzad <21FShahzad@heckgrammar.co.uk>
2024-07-09 15:15:15 +0100

    save 1

--- Program.cs
+++ Program.cs
@@ -18,31 +18,20 @@ namespace Y9_DEC_TO_BIN_SKELETON
             myString = myString + string1;
 
             //MAIN:  NUMBER CONVERSION PROGRAM
-            ///Console.WriteLine(numberConversion(13, 2));
-           
+
             //CODE GOES HERE
 
-           string binary = "";
-            int leftOVER = 0;
-           
-
             Console.WriteLine("Enter number");
-            int numb = Convert.ToInt32(Console.ReadLine());
-
-     
-            for (int i = 0;i < 8;i++)
-             {
-              leftOVER = numb % 2;
-              binary = leftOVER + binary;
-              numb =  numb / 2;
-               
-            }
-             Console.WriteLine(binary);
-
-
+            Console.WriteLine(Validation(string1));
+            Console.WriteLine(DenTOBIN(13, 2));
+            
+            
+           
         }
 
         
+
+
 
         //static void means the function will not return a value so it does not need a data type 
         //...this function DOES return a value so the method must have a data type
@@ -54,5 +43,42 @@ namespace Y9_DEC_TO_BIN_SKELETON
             string result = answer.ToString();
             return result; //REMOVE THE RED LINE!
         }
+
+
+        static string DenTOBIN(int n, int nbase) //STATIC STRINGS MEANS THE DATA TYPE BEING RETUNE IS A STRING
+        {
+            string binary = "";
+            int leftOVER = 0;
+            int numb = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 8; i++)
+            {
+                leftOVER = numb % 2;
+                binary = leftOVER + binary;
+                numb = numb / 2;
+
+            }
+           
+
+            string result = binary;
+            return result;
+        }
+
+
+
+        static string Validation(string input)
+        {
+            int numb = Convert.ToInt32(Console.ReadLine());
+            if (numb < -1 || numb > 256)
+            {
+                Console.WriteLine("Please enter a number between 0-255");
+                numb = Convert.ToInt32(Console.ReadLine());
+            }
+            
+           string result = Console.ReadLine();
+            return result;
+        }
+
     }
 }
+
